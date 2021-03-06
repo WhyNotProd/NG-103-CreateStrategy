f7b16ad277dda7a4e6bcb8d6339f97262c6f6e23
Dan McQuillan <djmcquillan76@gmail.com>
2021-03-06 15:05:53 -0800

    updates to routing and assets/strategie data plumbed up

--- src/navigation/router.tsx
+++ src/navigation/router.tsx
@@ -5,27 +5,33 @@ import Home from '../features/home';
 import Social from '../features/social';
 import HeatMap from '../features/heatmap';
 import Trends from '../features/trends';
-
+import Strategy from '../features/strategy';
+import CreateStrategy from '../features/strategy/create';
+import EditStrategy from '../features/strategy/edit';
 
 
 const Routes = (props) => {
-    // const user = "Test";
-    // const ProtectedRoute = ({component: Component, ...rest}) => (
-    //     user ? <Component {...rest} /> : <Redirect from="" to="login" />
-    // );
+    const user = "Test";
+    const ProtectedRoute = ({component: Component, ...rest}) => (
+        user ? <Component {...rest} /> : <Redirect from="" to="login" />
+    );
 
-    // const PublicRoute = ({component: Component, ...rest}) => (
-    //     <Component {...rest} />
-    // );
+    const PublicRoute = ({component: Component, ...rest}) => (
+        <Component {...rest} />
+    );
 
     // const history = createMemoryHistory();
     return (
         <Router>
             <Switch>
-                <Route path='/social'><Social/></Route>
-                <Route path='/trends'><Trends/></Route>
-                <Route path='/heatmap'><HeatMap/></Route>
-                <Route exact path='/'><Home/></Route>
+                <ProtectedRoute path='/social' component={Social}></ProtectedRoute>
+                <ProtectedRoute path='/strategy' component={Strategy}></ProtectedRoute>
+                <ProtectedRoute exact path='/strategy/:id' component={Strategy}></ProtectedRoute>
+                <ProtectedRoute path='/strategy/:id/edit' component={EditStrategy}></ProtectedRoute>
+                <ProtectedRoute path='/strategy/create' component={CreateStrategy}></ProtectedRoute>
+                <ProtectedRoute path='/trends' component={Trends}></ProtectedRoute>
+                <ProtectedRoute path='/heatmap' component={HeatMap}></ProtectedRoute>
+                <PublicRoute exact path='/' component={Home}></PublicRoute>
             </Switch>
         </Router>
     )
