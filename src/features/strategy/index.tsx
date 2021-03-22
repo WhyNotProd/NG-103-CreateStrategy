1202b76dfde535d6d657c87c52ac73e0f2c38ae6
dlukovskiy <dlukovskiy@nikait.co>
2021-03-23 01:38:23 +0300

    update

--- src/features/strategy/index.tsx
+++ src/features/strategy/index.tsx
@@ -1,32 +1,46 @@
 import React, {useEffect, useState} from 'react';
-import { useDispatch } from 'react-redux';
+import {connect, useDispatch} from 'react-redux';
 import { useParams } from "react-router-dom";
-import { getStrategies } from './strategySlice';
+import strategyReducer, { getStrategies } from './strategySlice';
 import { getAssets } from './assetSlice';
 import CreateStrategy from './create';
 import './strategyStyle.css';
 import SelectStrategy from "./select";
+import PropTypes from "prop-types";
+import {RootState} from "../../store/rootReducer";
 
-const Strategy = () => {
+
+const Strategy = (state: RootState) => {
 
   const [selectStrategy, setSelectStrategy] = useState(true)
   const dispatch = useDispatch();
   let params = useParams();
-  console.log(params);
   useEffect(() => {
     dispatch(getStrategies());
     dispatch(getAssets());
   }, [dispatch]);
 
+  const changeStrategy = () => {
+    setSelectStrategy(false)
+  }
   return (
     <div className="strategy">
+      {console.log(state, 'ttttttt')}
       {!selectStrategy ? (
           <CreateStrategy />
       ): (
-          <SelectStrategy />
+          <SelectStrategy closeSelectStrategy={changeStrategy} />
       )}
     </div>
   );
 };
 
-export default Strategy;
+Strategy.prototype = {
+  strategies: PropTypes.array
+}
+
+const mapStateToProps = state => ({
+  strategies: state.strategies,
+});
+
+export default connect(mapStateToProps, null)(Strategy);
